e3976f235342b6ff9101993b2b0a0e6486742735
Mallikarjuna Reddy Ravi <raaviarjun131@gmail.com>
2025-07-21 22:47:47 +0530

    Src Code EP-03

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-// const heading = React.createElement('h1',{id:'heading1'},"Hey Arjun Ready for the show.");
-// const root = ReactDOM.createRoot(document.getElementById('root'))
-// root.render(heading)
+import React from 'react';
+import ReactDOM from 'react-dom/client'
 
-const heading = React.createElement('div',{id:'grandParent'},
-    [React.createElement('h1',{},'Name:Arjun'),
-        React.createElement('p',{},'Occupation: Software Engineer'),
-        React.createElement('p',{},'Age: 25'),
-    ]
-)
-const root = ReactDOM.createRoot(document.getElementById('arjun'))
-root.render(heading)
+const Title = ()=>{
+    return <h1>Hey Arjun This is me</h1>
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<Title/>)
